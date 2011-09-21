c0a4b274e1fdfd22e1217e2bf70cc75c97ef3f72
Sebastian Kusnier <sebastian@kusnier.net>
2011-09-21 23:10:52 +0200

    add custom paths (CDPATH) (zshed ash version)

---
+++ home/oh-my-zsh.custom/paths.zsh
@@ -0,0 +1,2 @@
+# cd paths
+export CDPATH=.:~/devel:~/devel/xp.private/trunk/xp5/projects:~/devel/xp.private/trunk/xp5/worlds/:~/devel/xp.private/trunk/xp5/libs:~/devel/xp.private/trunk:/media/:etc/
