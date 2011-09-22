bb41cdc8693172fc5abb1a3baaa1df8681117e4e
Sebastian Kusnier <sebastian@kusnier.net>
2011-09-22 22:56:17 +0200

    add vim modeline

--- home/oh-my-zsh.custom/paths.zsh
+++ home/oh-my-zsh.custom/paths.zsh
@@ -1,2 +1,6 @@
+# -------------------------------------------------------------------------------------------------
+# vim: ft=zsh sw=2 ts=2 et
+# -------------------------------------------------------------------------------------------------
+#
 # cd paths
 export CDPATH=.:~/devel:~/devel/xp.private/trunk/xp5/projects:~/devel/xp.private/trunk/xp5/worlds/:~/devel/xp.private/trunk/xp5/libs:~/devel/xp.private/trunk:/media/:etc/
