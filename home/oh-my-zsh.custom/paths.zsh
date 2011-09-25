54312256c529fef334d7f9e5e069268bdac3fdf0
Sebastian Kusnier <sebastian@kusnier.net>
2011-09-25 20:31:34 +0200

    add brew paths

--- home/oh-my-zsh.custom/paths.zsh
+++ home/oh-my-zsh.custom/paths.zsh
@@ -1,6 +1,12 @@
 # -------------------------------------------------------------------------------------------------
 # vim: ft=zsh sw=2 ts=2 et
 # -------------------------------------------------------------------------------------------------
-#
+
+# brew path
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:$PATH
+export PATH=$PATH:$HOME/.gem/ruby/1.8/bin
+
 # cd paths
 export CDPATH=.:~/devel:~/devel/xp.private/trunk/xp5/projects:~/devel/xp.private/trunk/xp5/worlds/:~/devel/xp.private/trunk/xp5/libs:~/devel/xp.private/trunk:/media/:etc/
+
