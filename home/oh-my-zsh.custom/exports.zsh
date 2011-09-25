3ed34dcfffb763a6b3cc90fc8c5d7f725ffd2de3
Sebastian Kusnier <sebastian@kusnier.net>
2011-09-25 21:18:38 +0200

    add export script for zsh

---
+++ home/oh-my-zsh.custom/exports.zsh
@@ -0,0 +1,21 @@
+# History
+# Remove duplicate of previous line
+export HIST_IGNORE_DUPS=1
+# Don’t store lines starting with space
+export HIST_IGNORE_SPACE=1
+
+# Very long history file
+export HISTSIZE=5000000
+export HISTFILESIZE=$HISTSIZE
+
+# Mysql
+export MYSQL_PS1="\\d@\\h> "
+
+# Man
+# man pager: more info -> http://vim.wikia.com/wiki/Using_vim_as_a_man-page_viewer_under_Unix
+export MANPAGER="sh -c \"col -bx | iconv -c | view -c 'set ft=man nomod nolist titlestring=MANPAGE' -\""
+export PAGER=~/devel/gcode/scripts/bin/all/vimpager
+export MANPAGER=~/devel/gcode/scripts/bin/all/vimpager
+
+# cd options
+export FIGNORE='.svn'
