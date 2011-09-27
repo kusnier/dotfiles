89a82d6b7acd3cab1cbf5be95c98b4384b7e268f
Sebastian Kusnier <sebastian@kusnier.net>
2011-09-27 21:37:01 +0200

    obsolete f (find) function - use zsh globbing

--- home/oh-my-zsh.custom/functions.zsh
+++ home/oh-my-zsh.custom/functions.zsh
@@ -1,3 +1,7 @@
 function f () {
-  find . -not -name \*.svn-base -iname "$@"
+  #find . -not -name \*.svn-base -iname "$@"
+  tput setaf 9
+  echo "  find is busted"
+  echo "  use ls with zsh globbing!!!"
+  echo "  $ ls **/filename*"
 }
