5a647059182d9442300c4677768fcc259f450b0c
Sebastian Kusnier <sebastian@kusnier.net>
2011-10-02 09:36:06 +0200

    update autoreload

--- home/vimperator/plugin/autoreload.js
+++ home/vimperator/plugin/autoreload.js
@@ -2,37 +2,18 @@
 //
 // TODO: add some verbose feedback 
 // TODO: fix :stopautore command (needs more testing)?
-// TODO: autoreload in background (don't follow current tab)
-
-// set some global constants
+// DONE: autoreload in background (don't follow current tab)
 
 var ONESEC = 1000; // 1000ms
 var ONEMIN = 60 * ONESEC; // 60s
 var INTERVAL = ONEMIN; // 1m
 
-function AutoReloadPageVimperator(args)
-{
-    window.setTimeout(
-	    function()
-	    {
-		//window.location.reload() ; // WARNING: destroys all open tabs
-		// if (doc == getBrowser().contentDocument)
-		liberator.modules.tabs.reload(getBrowser().mCurrentTab) ;
-		// prevent infinite looping from happening
-		if (INTERVAL > 2000 )
-		{
-			AutoReloadPageVimperator(args) ;
-		}
-	    },
-	    INTERVAL
-    ) ;
-    //LastLoaded = new Date().toLocaleFormat("%x %X");
-    //liberator.echoerr("page last reloaded at: " + LastLoaded);
-}
+var _TAB_TO_AUTORELOAD;
+var _INTERVAL_ID; 
 
 // add vimperator command
 commands.addUserCommand(["autoreload"],
-	"start auto reload of current page every [N] seconds",
+	"start auto reload of current tab every [N] seconds",
 	function (args)
 	{ 
 		var rlimit = args.string;
@@ -47,13 +28,14 @@ commands.addUserCommand(["autoreload"],
 
 		if (rlimit && rlimit > 2)
 		{
-			liberator.echo("reloading page every " + rlimit + " seconds");
-			AutoReloadPageVimperator(rlimit);
+			_TAB_TO_AUTORELOAD = getBrowser().selectedTab;
+			liberator.echo("reloading every " + rlimit + " seconds tab with title: " + _TAB_TO_AUTORELOAD.label );
+			_INTERVAL_ID = window.setInterval( function(){ getBrowser().reloadTab( _TAB_TO_AUTORELOAD ); }, INTERVAL );
 		}
 		else
 		{
-			liberator.echoerr("unnacceptable interval. must be a miniumum of 3");
-			windows.setTimeout( function(){ ; },0); 
+			liberator.echoerr("unnacceptable interval. Must be a miniumum of 3 seconds");
+			//window.setTimeout( function(){ ; },0); 
 		}
 	},
 	{ argCount: "1" }
@@ -61,13 +43,10 @@ commands.addUserCommand(["autoreload"],
 
 // add vimperator command
 commands.addUserCommand(["stopautore"],
-	"stop auto reload of current page",
+	"stop auto reload of previously selected tab",
 	function (args)
 	{
-		INTERVAL = 0 * ONESEC; // how often to refresh page (in seconds)
-
-		liberator.echo("stopping autoreload of current page");
-		windows.setTimeout( function(){ ; },0); 
+		window.clearInterval( _INTERVAL_ID );
 	},
 	{ argCount: "0" }
 );
