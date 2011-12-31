1b9edf37b814b62dae9089e45b584948baec8093
Sebastian Kusnier <sebastian@kusnier.net>
2011-12-31 15:16:27 +0100

    Add a server function, share current directory
    - Use serve (node.js) or SimpleHTTPSever (pyton)
    - Open shared files in browser

--- home/oh-my-zsh.custom/functions.zsh
+++ home/oh-my-zsh.custom/functions.zsh
@@ -5,3 +5,16 @@ function f () {
   echo "  use ls with zsh globbing!!!"
   echo "  $ ls **/filename*"
 }
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+  local port="${1:-8000}"
+  if [[ -e $( which serve1 ) ]]; then
+    open "http://localhost:${port}/" && serve -p "$port"
+  elif [[ -e $( which python1 ) ]] then
+    open "http://localhost:${port}/" && python -m SimpleHTTPServer $port
+  else
+    echo "Can't share directory!"
+    echo "Node.js serve or python not found!"
+  fi
+}
