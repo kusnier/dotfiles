34daf577c2904b20624ca451a0368a245088989b
Sebastian Kusnier <sebastian@kusnier.net>
2012-01-06 10:07:44 +0100

    Remove test code

--- home/oh-my-zsh.custom/functions.zsh
+++ home/oh-my-zsh.custom/functions.zsh
@@ -9,9 +9,9 @@ function f () {
 # Start an HTTP server from a directory, optionally specifying the port
 function server() {
   local port="${1:-8000}"
-  if [[ -e $( which serve1 ) ]]; then
+  if [[ -e $( which serve ) ]]; then
     open "http://localhost:${port}/" && serve -p "$port"
-  elif [[ -e $( which python1 ) ]] then
+  elif [[ -e $( which python ) ]] then
     open "http://localhost:${port}/" && python -m SimpleHTTPServer $port
   else
     echo "Can't share directory!"
