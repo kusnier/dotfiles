0cdf1609c45cd0953e8c9d6e175ee4c049e5b966
Sebastian Kusnier <sebastian@kusnier.net>
2012-03-10 12:44:30 +0100

    Merge all files in vim/bundle to one directory
    - This directory will be later on a ram disk

---
+++ speedvim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ~/.vim/bundle
+mkdir ~/.vim/bundle.merged;
+rm -r ~/.vim/bundle.merged/*
+for d in *;do
+  cd $d
+  cp -i -r * ~/.vim/bundle.merged
+# Cp is better. I will move my vim files to a ramdisk
+#  for f in **/*;do
+#    t="$HOME/.vim/bundle.merged/$(dirname "$f")";
+#    test -d "$t" || mkdir -p "$t";
+#    ln "$f" "$t";
+#  done;
+  cd ..;
+done
+
