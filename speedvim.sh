c3d26c121b6a57e8c78a842f212f640bac59b71a
Sebastian Kusnier <sebastian@kusnier.net>
2012-03-11 14:35:16 +0100

    Add two modes
    - copy mode
    - hardlink mode

--- speedvim.sh
+++ speedvim.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
+MODE="copy"
+
 mkdir home/vim/bundle.merged;
 rm -r home/vim/bundle.merged/*
 cd home/vim/bundle
 for d in *;do
   cd $d
-  cp -i -r * ../../bundle.merged
-# Cp is better. I will move my vim files to a ramdisk
-#  for f in **/*;do
-#    t="$HOME/.vim/bundle.merged/$(dirname "$f")";
-#    test -d "$t" || mkdir -p "$t";
-#    ln "$f" "$t";
-#  done;
+
+  if [ $MODE == 'copy' ]; then
+    #############################
+    # Solution 1: Copy files
+    cp -i -r * ../../bundle.merged
+  fi
+
+  if [ $MODE == 'hardlink' ]; then
+    #############################
+    # Solution 2: Hardlink files
+    #   SRC directory is root of tree you want to replicate.
+    #   It should *not* be an absolute pathname.
+    SRC=.
+    DSTDIR=../../bundle.merged
+
+    # Replicate directory structure
+    find $SRC -type d -exec mkdir -p $DSTDIR/{} \;
+
+    # Make hard links to files
+    find $SRC -type f -exec ln {} $DSTDIR/{} \;
+  fi
+
   cd ..;
 done
 
 # Generate doc/tags file
 vim -c ':helptags ../bundle.merged/doc/' -c ':q'
+
+
+
+
+
