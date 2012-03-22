31fddc1b90c100e9f5beb285e63bcabe8b92f942
Sebastian Kusnier <sebastian@kusnier.net>
2012-03-22 20:45:01 +0100

    Add useful functions
    - Source: mathiasbynens/dotfiles

--- home/oh-my-zsh.custom/functions.zsh
+++ home/oh-my-zsh.custom/functions.zsh
@@ -6,3 +6,13 @@ function f () {
   echo "  $ ls **/filename*"
 }
 
+# Create a data URL from an image (works for other file types too, if you tweak the Content-Type afterwards)
+dataurl() {
+echo "data:image/${1##*.};base64,$(openssl base64 -in "$1")" | tr -d '\n'
+}
+
+# Gzip-enabled `curl`
+function gurl() {
+curl -sH "Accept-Encoding: gzip" "$@" | gunzip
+}
+
