e796150a11595fb4bac1c06084fdd33421838cd6
Sebastian Kusnier <sebastian@kusnier.net>
2012-03-22 20:52:47 +0100

    Remove f function
    - This function was oly a hint to not use a old f alias

--- home/oh-my-zsh.custom/functions.zsh
+++ home/oh-my-zsh.custom/functions.zsh
@@ -1,11 +1,3 @@
-function f () {
-  #find . -not -name \*.svn-base -iname "$@"
-  tput setaf 9
-  echo "  find is busted"
-  echo "  use ls with zsh globbing!!!"
-  echo "  $ ls **/filename*"
-}
-
 # Create a data URL from an image (works for other file types too, if you tweak the Content-Type afterwards)
 dataurl() {
 echo "data:image/${1##*.};base64,$(openssl base64 -in "$1")" | tr -d '\n'
