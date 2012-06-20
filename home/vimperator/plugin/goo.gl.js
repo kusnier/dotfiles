f88a0c7dddba8fd5af7e0a260d2ba0d31220714d
Sebastian Kusnier <sebastian@kusnier.net>
2012-06-20 14:03:51 +0200

    Update goo.gl.js to version 0.2

--- home/vimperator/plugin/goo.gl.js
+++ home/vimperator/plugin/goo.gl.js
@@ -1,55 +1,64 @@
-// INFO //
+// {{{ INFO
 var INFO =
-<plugin name="goo.gl.js" version="0.1"
+<plugin name="goo.gl.js" version="0.2"
+        summary="google url shortener"
         href="http://github.com/vimpr/vimperator-plugins/blob/master/goo.gl.js"
-        summary="Get shorten URL by goo.gl"
         xmlns="http://vimperator.org/namespaces/liberator">
   <author email="mitsugu.oyama@gmail.com">Mitsugu Oyama</author>
-  <author email="anekos@snca.net">anekos</author>
   <license href="http://opensource.org/licenses/mit-license.php">MIT</license>
-  <project name="Vimperator" minVersion="2.3"/>
-  <p>
-    You can get short URL by goo.gl by this plugin.
-  </p>
+  <project name="Vimperator" minVersion="3.3"/>
+  <p>Shorten URL by used of google</p>
   <item>
-    <tags>'goo.gl'</tags>
-    <spec>:googl <oa>Long URL</oa></spec>
+    <tags>'googlUrlShortener'</tags>
+    <spec>:googlUrlShortener</spec>
     <description>
-      <p>You can get short URL by <link topic="http://goo.gl/">goo.gl</link> by this plugin.</p>
+      <p>Shorten URL by used of google</p>
     </description>
   </item>
 </plugin>;
+// }}}
 
-(function(){
-  let short_url=function(long_url){
-    let req=new XMLHttpRequest();
-    req.addEventListener("load",function(){
-      let response=JSON.parse(req.responseText);
-      liberator.echo(response.short_url);
-      util.copyToClipboard(response.short_url,true);
-    },false);
-    req.addEventListener("error",function(){
-      liberator.echo("Responce errror status from goo.gl. Status Code:" + req.status);
-    },false);
-    req.open("POST", "http://goo.gl/api/shorten?url="+encodeURIComponent(long_url));
-    req.setRequestHeader("X-Auth-Google-Url-Shortener","true");
-    req.send();
-  };
-  commands.addUserCommand(
-    ["googl"],
-    "Get short URL from Google",
-    function(args){
-      let long_url;
-      if(args.length==0){
-        long_url=buffer.URL;
-      }else if(args.length==1){
-        long_url=args.literalArg;
-      }else{
-        liberator.echoerr('argument error');
-        return;
-      }
-      short_url(long_url);
-    }, {
-      literal: 0
-    }, true);
-})();
+commands.addUserCommand(
+  ['googleUrlShortener'],
+  'google url shortener',
+  function(){
+// {{{ environment
+    let contents=gBrowser.selectedBrowser.contentDocument;
+    const endPoint='https://www.googleapis.com/urlshortener/v1/url';
+    const contentType='application/json';
+    let xhr;
+// }}}
+
+// {{{ edit to json from long URL
+    let tmp={};
+    tmp.longUrl=contents.URL;
+    let jsonString=JSON.stringify(tmp);
+//}}}
+
+// {{{ convert success
+    function getData(){
+      let ret=JSON.parse(xhr.responseText);
+      liberator.echo('Shorten URL to \"'+ret.id+'\"');
+      util.copyToClipboard(ret.id);
+    }
+// }}}
+
+// {{{ false convert
+    function requestError(){
+      liberator.echoerr('cannot convert by used google url shortener');
+      return false;
+    }
+// }}}
+
+// {{{ XMLHttpRequest
+    xhr=new XMLHttpRequest();
+    xhr.addEventListener("load",getData,false);
+    xhr.addEventListener("error",requestError,false);
+    xhr.open("POST",endPoint);
+    xhr.setRequestHeader('Content-Type',contentType);
+    xhr.send(jsonString);
+//  }}}
+  },
+  {},
+  true
+);
