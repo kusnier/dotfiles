fd39ec49f77d2be9987ffcf5d6785b9ffe448289
Sebastian Kusnier <sebastian@kusnier.net>
2012-07-01 09:57:53 +0200

    Remove 'which vimpager' test
    - At this point the path is not fully set

--- home/oh-my-zsh.custom/exports.zsh
+++ home/oh-my-zsh.custom/exports.zsh
@@ -13,9 +13,9 @@ export MYSQL_PS1="\\d@\\h> "
 
 # Man
 # man pager: more info -> http://vim.wikia.com/wiki/Using_vim_as_a_man-page_viewer_under_Unix
-export MANPAGER="sh -c \"col -bx | iconv -c | view -c 'set ft=man nomod nolist titlestring=MANPAGE' -\""
-[[ -x `which vimpager` ]] && export PAGER=vimpager
-[[ -x `which vimpager` ]] && export MANPAGER=vimpager
+#export MANPAGER="sh -c \"col -bx | iconv -c | view -c 'set ft=man nomod nolist titlestring=MANPAGE' -\""
+export PAGER=vimpager
+export MANPAGER=vimpager
 
 # cd options
 export FIGNORE='.svn'
