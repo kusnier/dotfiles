9549cc5db65dc521acfc9ec29a03985d72b0d1dd
Sebastian Kusnier <sebastian@kusnier.net>
2012-09-07 13:45:05 +0200

    Add function for xp code style clearance

--- home/oh-my-zsh.custom/functions.zsh
+++ home/oh-my-zsh.custom/functions.zsh
@@ -72,6 +72,49 @@ xxd $1 | vipe | xxd -r | sponge $1
 }
 
 # Trailing whitespace elimination function for multiple files
-function removetrailingspaces() {
-find . -type f -name "$1" -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+function xpcs_removetrailingspaces() {
+  find . -type f -name "$1" -exec sed --in-place 's/[[:space:]]\+$//' {} \+
+}
+
+# xp:cs: No two consecutive empty lines
+function xpcs_no_two_consecutive_empty_lines() {
+  find . -type f -name "$1" -exec sed --in-place '/^$/N;/^\n$/D' {} \+
+}
+
+# xp:cs: One space after operator
+function xpcs_one_space_after_operator() {
+  find . -type f -name "$1" -exec sed --in-place 's/\(\S\)\([+<=]=\{1,2\}\)\(\S\)/\1 \2 \3/' {} \+
+}
+
+# xp:cs: One space after keyword
+function xpcs_one_space_after_keyword() {
+  foreach type ('throw' 'if' 'while' 'switch' 'for' 'foreach' );
+    find . -type f -name "$1" -exec sed --in-place "s/\($type\)\((\)/\1 \2/" {} \+
+  end
+}
+
+# xp:cs: Empty line before comment line
+function xpcs_empty_line_before_comment_line() {
+  find . -type f -name "$1" -exec vim +'set autowrite' +':bufdo XpcsEmptyLineBeforeCommentLine' {} \+
+}
+
+# xp:cs: One space after comma
+function xpcs_one_space_after_comma() {
+  find . -type f -name "$1" -exec sed --in-place 's/array(\(\w\+\),\(\w\+\)/array(\1, \2)/' {} \+
+}
+
+# svn
+
+# show for each file a diff in vim
+function svn_gdiff {
+  foreach f (`svn st | cut -d' ' -f 8`); gvim -geometry 500x500 -f $f +':normal ,cv'; end
+}
+
+# show for each file a diff in vim
+function svn_gdiff_all {
+  gvim -geometry 500x500 --servername svn_diff
+  foreach f (`svn st | cut -d' ' -f 8`);
+    gvim --servername svn_diff --remote-tab $f
+  end
+  gvim --servername svn_diff --remote-send '<ESC>:tabdo :VCSVimDiff<CR>'
 }
