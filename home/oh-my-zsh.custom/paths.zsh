b4c5ea74f2c68cd1f8a65cd0e7bfdc099864ecc5
Sebastian Kusnier <sebastian@kusnier.net>
2012-10-03 21:40:55 +0200

    Add npm/bin to path

--- home/oh-my-zsh.custom/paths.zsh
+++ home/oh-my-zsh.custom/paths.zsh
@@ -3,6 +3,9 @@ export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=$PATH:$HOME/.gem/ruby/1.8/bin
 
+# npm path
+export PATH=/usr/local/share/npm/bin:$PATH
+
 # cd paths
 export CDPATH=.:~/devel:~/devel/xp.private/trunk/xp5/projects:~/devel/xp.private/trunk/xp5/worlds/:~/devel/xp.private/trunk/xp5/libs:~/devel/xp.private/trunk:/media/:etc/
 
