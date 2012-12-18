ebefeacd7c3315393a54c12619cfe1ee070271cb
Sebastian Kusnier <sebastian@kusnier.net>
2012-12-18 21:37:02 +0100

    Add vimode status to the prompt

---
+++ home/oh-my-zsh.custom/vimode.zsh
@@ -0,0 +1,11 @@
+function zle-line-init zle-keymap-select {
+    local normal insert
+    insert="%{%K{yellow}%}%{%F{black}%}"
+    normal="%{%K{green}%}%{%F{black}%}"
+    RPS1="${${KEYMAP/vicmd/${normal}[N]}/(main|viins)/${insert}[I]}"
+    RPS2=$RPS1
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
