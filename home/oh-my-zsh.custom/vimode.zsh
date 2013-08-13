e086c406e8779f6a6530bd2542e300a0715a24ec
Sebastian Kusnier <sebastian@kusnier.net>
2013-08-13 17:48:04 +0200

    Improve vi-mode status

--- home/oh-my-zsh.custom/vimode.zsh
+++ home/oh-my-zsh.custom/vimode.zsh
@@ -1,11 +1,14 @@
 function zle-line-init zle-keymap-select {
     local normal insert
-    insert="%{%K{226}%}%{%F{black}%}"
-    normal="%{%K{green}%}%{%F{black}%}"
-    RPS1="${${KEYMAP/vicmd/${normal}[N]}/(main|viins)/${insert}[I]}"
+    insert="%{$fg[yellow]%}%{$fg[black]$bg[yellow]%} INSERT %{$reset_color%}"
+    normal="%{$fg[green]%}%{$fg[black]$bg[green]%} COMMAND %{$reset_color%}"
+    RPS1="${${KEYMAP/vicmd/${normal}}/(main|viins)/${insert}}"
     RPS2=$RPS1
     zle reset-prompt
 }
 
 zle -N zle-line-init
 zle -N zle-keymap-select
+
+# Remove any right prompt, show only the last mode
+setopt transientrprompt
