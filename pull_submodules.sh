baf014ccf6fd3b8f5d539938749446587bc2fa53
Sebastian Kusnier <sebastian@kusnier.net>
2013-09-03 22:08:56 +0200

    Use other branch for powerline

--- pull_submodules.sh
+++ pull_submodules.sh
@@ -5,4 +5,4 @@ git submodule foreach git submodule update --init
 git submodule foreach git pull
 
 # omg. why a development branch and a empty master?
-git submodule foreach '[ "$path" = "home/vim/bundle/vim-powerline" ] && branch=develop || branch=master; git checkout $branch'
+git submodule foreach '[ "$path" = "libs/powerline" ] && branch=develop || branch=master; git checkout $branch'
