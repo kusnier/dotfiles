3d4c261827f5524067d41043f756c66a9ec60874
Sebastian Kusnier <sebastian@kusnier.net>
2017-05-25 21:40:35 +0200

    Set description for dataurl function

--- fish/functions/dataurl.fish
+++ fish/functions/dataurl.fish
@@ -1,4 +1,4 @@
-function dataurl -d "Create data url from image"
+function dataurl --description 'Create a data URL from an image (works for other file types too, if you tweak the Content-Type afterwards)'
 	set filename $argv[1]
     set extension (echo $filename | sed 's/^.*\.//')
     set base64 (openssl base64 -in "$filename")
