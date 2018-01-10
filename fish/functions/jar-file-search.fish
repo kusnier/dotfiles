79cbe32612ef0403d28ec9ff4584e7231e43623b
Sebastian Kusnier <sebastian@kusnier.net>
2018-01-10 13:18:33 +0100

    Make script working with fish shell

--- fish/functions/jar-file-search.fish
+++ fish/functions/jar-file-search.fish
@@ -1,6 +1,7 @@
+# Defined in /tmp/fish.HHtHTK/jar-file-search.fish @ line 2
 function jar-file-search
-	set PATTERN $argv[1]
-    #for f in `ls **/*.jar`; do
-    #   echo "$f: "; unzip -l $f | grep "$PATTERN";
-    #done
+	set PATTERN $argv
+    for f in (ls ***.jar)
+       echo "$f: "; unzip -l $f | grep "$PATTERN";
+    end
 end
