3e91aa6a9718417b9217f65fd053a17ae4f95270
Sebastian Kusnier <sebastian@kusnier.net>
2018-09-17 12:28:57 +0200

    Update url

--- fish/functions/m-release-admin-app.fish
+++ fish/functions/m-release-admin-app.fish
@@ -1,6 +1,6 @@
+# Defined in /tmp/fish.xESz3L/m-release-admin-app.fish @ line 2
 function m-release-admin-app
-
-    set URL "$JENKINS_RELEASE_URL/Release%20admin-app"
+	set URL "Release%20admin-app"
 
     m-release-job-url $URL
 end
--- fish/functions/m-release-vaadin-ui.fish
+++ fish/functions/m-release-vaadin-ui.fish
@@ -1,6 +1,6 @@
+# Defined in /tmp/fish.XguxEh/m-release-vaadin-ui.fish @ line 2
 function m-release-vaadin-ui
-
-    set URL "$JENKINS_RELEASE_URL/Release%20vaadin%20ui%20framework""
+	set URL "Release%20vaadin%20ui%20framework"
 
     m-release-job-url $URL
 end
