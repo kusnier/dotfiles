a3f70485e14ac9586ebab94269a0de88d3ae62bc
Sebastian Kusnier <sebastian@kusnier.net>
2020-06-29 10:58:54 +0200

    Update path

--- fish/functions/m-release-common-component.fish
+++ fish/functions/m-release-common-component.fish
@@ -1,6 +1,6 @@
-# Defined in /home/kuse/.config/fish/functions/m-release-common-component.fish @ line 2
+# Defined in /tmp/fish.qNpeGS/m-release-common-component.fish @ line 2
 function m-release-common-component
-	set URL "portal-common-component.trunk"
+	set URL "job/next/job/common-component/job/master"
 
     m-release-job-url $URL
 end
