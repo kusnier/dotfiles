37166db12711abf6132f224fa979b180bc2fe971
Sebastian Kusnier <sebastian@kusnier.net>
2020-10-19 09:40:16 +0200

    Update url of job

--- fish/functions/m-release-vaadin-ui.fish
+++ fish/functions/m-release-vaadin-ui.fish
@@ -1,6 +1,6 @@
-# Defined in /tmp/fish.WRsxCg/m-release-vaadin-ui.fish @ line 2
+# Defined in /tmp/fish.V3rJ7g/m-release-vaadin-ui.fish @ line 2
 function m-release-vaadin-ui
-	set URL "portal-vaadin-ui-parent.trunk"
+	set URL "/job/next/job/vaadin-ui/job/master"
 
     m-release-job-url $URL
 end
