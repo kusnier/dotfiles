dd102ba9e87661011834e1687e09c1a27c24c5e5
Sebastian Kusnier <sebastian@kusnier.net>
2024-05-14 14:24:34 +0200

    Set new font

--- fish/functions/fcowlol.fish
+++ fish/functions/fcowlol.fish
@@ -1,3 +1,3 @@
 function fcowlol -d "Show a fun twitch banner" --wraps "figlet"
-    figlet -f ogre -w9999 $argv | cowsay -W 9999 -n -p | lolcat
+    figlet -f smslant -w9999 $argv | cowsay -W 9999 -n -p | lolcat
 end
